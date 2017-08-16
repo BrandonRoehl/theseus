27b5e6eba779662514de5b05a31d64566edd87ec
Brandon Roehl <RoehlBG8339@uwec.edu>
2017-08-16 14:57:20 -0500

    Fallback err

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_model, only: [:get]
 
   def console
   end
 
   def get
-      render json: params[:model].camelize.constantize.to_json
+      render json: @model.to_json
+  rescue
+      render plain: 'No model can be found by that name'
+  end
+
+  private
+
+  def set_model
+    @model = params[:model].camelize.constantize
   end
 end
