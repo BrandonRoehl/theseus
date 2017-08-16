36cab365b1b871d5994ad788ad6c87f5b0930d83
Brandon Roehl <RoehlBG8339@uwec.edu>
2017-08-16 12:29:03 -0500

    shcema

---
+++ db/migrate/20170816172425_create_instances.rb
@@ -0,0 +1,11 @@
+class CreateInstances < ActiveRecord::Migration[5.1]
+  def change
+    create_table :instances do |t|
+      t.string :key
+      t.text :value
+      t.references :model, foreign_key: true, null: false
+
+      t.index [:key, :model_id], unique: true
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,27 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20170816172425) do
+
+  create_table "instances", force: :cascade do |t|
+    t.string "key"
+    t.text "value"
+    t.integer "model_id", null: false
+    t.index ["key", "model_id"], name: "index_instances_on_key_and_model_id", unique: true
+    t.index ["model_id"], name: "index_instances_on_model_id"
+  end
+
+  create_table "models", force: :cascade do |t|
+    t.string "name"
+  end
+
+end
